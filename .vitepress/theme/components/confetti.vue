9a283272921cb152e773bc8b1e3137037a83dc39
UUUUUUII <81506591+UUUUUUII@users.noreply.github.com>
2025-08-10 14:27:28 +0800

    feat : particle effects

---
+++ .vitepress/theme/components/confetti.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { onMounted } from "vue";
+import confetti from "canvas-confetti";
+const randomInRange = (min: number, max: number) => {
+  return Math.random() * (max - min) + min;
+};
+onMounted(() =>
+  confetti({
+    angle: randomInRange(55, 125),
+    spread: randomInRange(50, 70),
+    particleCount: randomInRange(50, 100),
+    origin: { y: 0.6 },
+  })
+);
+</script>
--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,23 +1,19 @@
 // https://vitepress.dev/guide/custom-theme
-// import { h } from 'vue'
-// import type { Theme } from 'vitepress'
-// import DefaultTheme from 'vitepress/theme'
-// import './style.css'
+import { h } from "vue";
+import type { Theme } from "vitepress";
+import DefaultTheme from "vitepress/theme";
+import "./style.css";
+import "./custom.css";
+import confetti from "./components/confetti.vue";
 
-// export default {
-//   extends: DefaultTheme,
-//   Layout: () => {
-//     return h(DefaultTheme.Layout, null, {
-//       // https://vitepress.dev/guide/extending-default-theme#layout-slots
-//     })
-//   },
-//   enhanceApp({ app, router, siteData }) {
-//     // ...
-//   }
-// } satisfies Theme
-
-import DefaultTheme from 'vitepress/theme';
-import './style.css'
-import './custom.css';
-
-export default DefaultTheme;
+export default {
+  extends: DefaultTheme,
+  Layout: () => {
+    return h(DefaultTheme.Layout, null, {
+      // https://vitepress.dev/guide/extending-default-theme#layout-slots
+    });
+  },
+  enhanceApp({ app, router, siteData }) {
+    app.component("confetti", confetti);
+  },
+} satisfies Theme;
